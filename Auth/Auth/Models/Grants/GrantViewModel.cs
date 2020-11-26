eb511dc4b586333b3f88fc7afa728656f1df987b
Quan Truong <trongquanuit@gmail.com>
2020-11-26 16:54:31 +0700

    Up

---
+++ Auth/Auth/Models/Grants/GrantViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Auth.Models.Grants
+{
+    public class GrantViewModel
+    {
+        public string ClientId { get; set; }
+        public string ClientName { get; set; }
+        public string ClientUrl { get; set; }
+        public string ClientLogoUrl { get; set; }
+        public string Description { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Expires { get; set; }
+        public IEnumerable<string> IdentityGrantNames { get; set; }
+        public IEnumerable<string> ApiGrantNames { get; set; }
+    }
+}
